ad6a172cb277964fa36d6e2ac6d60a21750bbba5
@vortiz-agropago <vortiz@agropago.com>
2023-12-26 17:17:18 -0300

    CHALLENGE-011 (VORTIZ) - feat  : ceate guard + decorator + util

---
+++ src/utils/auth.util.ts
@@ -0,0 +1,18 @@
+import * as jwt from 'jsonwebtoken';
+import { AuthTokenResult, IUseToken } from 'src/interfaces/auth.interface';
+
+export const useToken = (token: string): IUseToken | string => {
+  try {
+    const decode = jwt.decode(token) as AuthTokenResult;
+
+    const currentDate = new Date();
+    const expiresDate = new Date(decode.exp);
+
+    return {
+      sub: decode.sub,
+      isExpired: +expiresDate <= +currentDate / 1000,
+    };
+  } catch (error) {
+    return 'Token is invalid';
+  }
+};
