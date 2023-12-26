e305862eff7d08b0c8addc6a793b6dc91a0e683a
@vortiz-agropago <vortiz@agropago.com>
2023-12-26 11:08:51 -0300

    CHALLENGE-005 (VORTIZ) - feat  : create develop env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from './db/config';
+import { ConfigModule } from '@nestjs/config';
+
+import { DataSourceConfig } from './db/config';
 
 @Module({
-  imports: [UsersModule, ReservationsModule, TypeOrmModule.forRoot(config)],
+  imports: [
+    UsersModule,
+    ReservationsModule,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
+  ],
   controllers: [],
   providers: [],
 })
