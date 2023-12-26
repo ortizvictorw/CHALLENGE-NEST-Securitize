a269b1196e870ac859acdb52cbcff01dd91d3ee4
@vortiz-agropago <vortiz@agropago.com>
2023-12-26 11:35:45 -0300

    CHALLENGE-006 (VORTIZ) - feat  : create dto and entities user

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('CHALLENGE SECURITIZE API')
