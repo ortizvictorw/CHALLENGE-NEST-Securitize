5b8bc8e2c39801c6be39a0597eb3e1a4c4df635b
@vortiz-agropago <vortiz@agropago.com>
2023-12-27 00:23:24 -0300

    CHALLENGE-012 (VORTIZ) - feat  : create services and refactor dto

--- src/reservations/controllers/reservations.controller.ts
+++ src/reservations/controllers/reservations.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ReservationsService } from '../services/reservations.service';
+import { ReservationDTO } from '../dto/reservation.dto';
+import { Request } from 'express';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @ApiTags('Reservations')
 @Controller('reservations')
+@UseGuards(AuthGuard)
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Post()
+  public async saveReservations(
+    @Body() body: ReservationDTO,
+    @Req() req: Request,
+  ) {
+    body.userId = +req.idUser;
+    return await this.reservationsService.save(body);
+  }
 }
--- src/reservations/services/reservations.service.ts
+++ src/reservations/services/reservations.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ReservationDTO } from '../dto/reservation.dto';
+import { ReservationsEntity } from '../entities/reservation.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReservationsService {}
+export class ReservationsService {
+  constructor(
+    @InjectRepository(ReservationsEntity)
+    private readonly reservationRepository: Repository<ReservationsEntity>,
+  ) {}
+  public async save(body: ReservationDTO): Promise<ReservationsEntity> {
+    try {
+      return await this.reservationRepository.save(body);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}
