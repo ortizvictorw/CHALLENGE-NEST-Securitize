2d5e1458678b44863b73ee61500b2265d283d1ce
@vortiz-agropago <vortiz@agropago.com>
2023-12-27 14:00:43 -0300

    CHALLENGE-016 (VORTIZ) - feat  : install and appy cors and morgan

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('combined'));
+
+  app.use(cors());
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +19,7 @@ async function bootstrap() {
       },
     }),
   );
+
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
   app.setGlobalPrefix('api');
@@ -27,4 +34,5 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
